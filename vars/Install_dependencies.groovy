f24c39697eb996029f270a3c9e543d236052cf06
shreya1396 <42531964+shreya1396@users.noreply.github.com>
2018-09-24 13:28:27 +0530

    Create Install_dependencies.groovy

---
+++ vars/Install_dependencies.groovy
@@ -0,0 +1,27 @@
+def call(body) {
+
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+
+        node {
+            // Clean workspace before doing anything
+            deleteDir()
+
+            try {
+                stage ('Clone') {
+                    checkout scm
+                }
+                stage ('Install-go') {
+                    sh "apt-get update"
+                    sh "apt-get install go"
+                    sh "apt-get install maven"
+                }
+                
+            } catch (err) {
+                currentBuild.result = 'FAILED'
+                throw err
+            }
+        }
+    }
